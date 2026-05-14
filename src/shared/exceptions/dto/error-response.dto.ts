8789e8506f4f295b22b767edfd95f2f4b004888c
Jean Carlos Lerma <yancalerma@gmail.com>
2026-05-14 13:47:50 -0500

    feat: se implemento el global exception filter (#2)

---
+++ src/shared/exceptions/dto/error-response.dto.ts
@@ -0,0 +1,11 @@
+export class ErrorResponseDto {
+   statusCode!: number;
+   error!: string;
+   message!: string | string[];
+   timestamp!: string;
+   path!: string;
+
+   constructor(partial: Partial<ErrorResponseDto>) {
+      Object.assign(this, partial);
+   }
+}
